5d539d96932b0e0e4a77bdd40f67050a75b30ee2
kizombaDev <marcel.swoboda@gmx.de>
2017-04-09 18:53:14 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-
-#configureTravis
-
-case "$TARGET" in
-
-BUILD)
-
   # Hack to keep job alive even if no logs during more than 10 minutes.
   # That can occur when uploading sonarqube.zip to Artifactory.
   ./clock.sh &
@@ -30,40 +23,32 @@ BUILD)
     # For this reason errors are ignored with "|| true"
     git fetch --unshallow || true
          
-   mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar
+    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar
 
   elif [[ "$TRAVIS_BRANCH" == "branch-"* ]] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo 'Build release branch'
 
-    mvn deploy $MAVEN_ARGS -Pdeploy-sonarsource,release
+    mvn package $MAVEN_ARGS 
 
   elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN:-}" ]; then
     echo 'Build and analyze internal pull request'
 
     mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar #\
-    #    $MAVEN_ARGS \
-     #   -Dsource.skip=true \
-      #  -Pdeploy-sonarsource \
-       # -Dsonar.analysis.mode=preview \
-       # -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-       # -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-       # -Dsonar.github.oauth=$GITHUB_TOKEN \
-       # -Dsonar.host.url=$SONAR_HOST_URL \
-       # -Dsonar.login=$SONAR_TOKEN
+        $MAVEN_ARGS \
+        -Dsource.skip=true \
+        -Pdeploy-sonarsource \
+        -Dsonar.analysis.mode=preview \
+        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+        -Dsonar.github.oauth=$GITHUB_TOKEN \
+        -Dsonar.host.url=$SONAR_HOST_URL \
+        -Dsonar.login=$SONAR_TOKEN
 
   else
     echo 'Build feature branch or external pull request'
 
     mvn install $MAVEN_ARGS -Dsource.skip=true
   fi
-  ;;
-
-*)
-  echo "Unexpected TARGET value: $TARGET"
-  exit 1
-  ;;
-
-esac
 
 #stop the clock
 touch stop
