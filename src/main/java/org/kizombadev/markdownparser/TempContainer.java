b13b8f97ea565ce525e70602b7418c7057d0af7e
Marcel Swoboda <marcel.swoboda@gmx.de>
2017-04-14 12:49:50 +0200

    move tempContainer

--- src/main/java/org/kizombadev/markdownparser/TempContainer.java
+++
@@ -1,7 +0,0 @@
-package org.kizombadev.markdownparser;
-
-import org.kizombadev.markdownparser.entities.Syntax;
-
-
-public class TempContainer extends Syntax {
-}
---
+++ src/main/java/org/kizombadev/markdownparser/entities/TempContainer.java
@@ -0,0 +1,6 @@
+package org.kizombadev.markdownparser.entities;
+
+
+
+public class TempContainer extends Syntax {
+}
